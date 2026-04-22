21c45592730616e310fa9ff26f62fd027d36c9ed
yonyonhee <doole3488@gmail.com>
2026-04-22 17:54:34 +0900

    tockk 0.1.1

--- Casks/tockk.rb
+++ Casks/tockk.rb
@@ -1,8 +1,8 @@
 cask "tockk" do
-  version "0.1.0"
-  sha256 "b10b0cd2647ed02203a430495fdb4fd0e690913bd8131d06e352dffcc5184127"
+  version "0.1.1"
+  sha256 "b8eb89569bfad0d54519d3eeb0be09858b72e2e61059eb73f418dca9623f6c25"
 
-  url "https://github.com/somee4/tockk/releases/download/v0.1.0/Tockk-0.1.0.dmg",
+  url "https://github.com/somee4/tockk/releases/download/v0.1.1/Tockk-0.1.1.dmg",
       verified: "github.com/somee4/tockk/"
   name "Tockk"
   desc "Notch-style notifications for local AI coding agent events"
